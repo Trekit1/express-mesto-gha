cbe40779d8c8012b5ea684dfa32e8e483da27ea3
Kirill Gurov <gurov1997@gmail.com>
2022-09-02 16:56:54 +0200

    test2

--- app.js
+++ app.js
@@ -23,9 +23,6 @@ app.use(express.json());
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/https?:\/\/(www\.)?([a-zA-Z0-9-._~:/?#[]@!\$&'()\*\+,;=]#?)/),
     email: Joi.string().required(),
     password: Joi.string().required(),
   }),
