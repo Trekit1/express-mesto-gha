59110480511a4b8d7344b30cb38e789c9e009184
Kirill Gurov <gurov1997@gmail.com>
2022-09-03 20:15:48 +0200

    feat: добавил email и password к схеме

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const validator = require('validator');
+
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -17,6 +19,14 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      validate: { validator: validator.isEmail },
+    },
+    password: {
+      type: String,
     },
   },
   { versionKey: false },
