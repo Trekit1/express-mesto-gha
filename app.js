17f018b478a755bd013b5ced36254b19c3196ef0
Kirill Gurov <gurov1997@gmail.com>
2022-09-04 21:14:28 +0200

    тестируем

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regexregex(/https?:\/\/(www\.)?([a-zA-Z0-9-._~:/?#@!$&'()+,;=]*)\.([a-zA-Z])#?/),
+    avatar: Joi.string().regex(/https?:\/\/(www\.)?([a-zA-Z0-9-._~:/?#@!$&'()+,;=]*)\.([a-zA-Z])#?/),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
--- routes/cards.js
+++ routes/cards.js
@@ -9,7 +9,7 @@ const { dislikeCard } = require('../controllers/cards');
 routerCard.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().regexregex(/https?:\/\/(www\.)?([a-zA-Z0-9-._~:/?#@!$&'()+,;=]*)\.([a-zA-Z])#?/),
+    link: Joi.string().required().regex(/https?:\/\/(www\.)?([a-zA-Z0-9-._~:/?#@!$&'()+,;=]*)\.([a-zA-Z])#?/),
   }),
 }), createCard);
 routerCard.get('/', getAllCards);
